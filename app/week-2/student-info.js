ecc80c89d9d0f2272ac3793ab25f380aa64fa939
ishamjotkaur <ishamjotkaur17@gmail.com>
2025-01-21 10:42:37 -0700

    commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+export default function studentInfo () {
+      
+    return (
+        <main>
+            <div>
+                <p> Ishamjot Kaur</p>
+                <Link href="https://github.com/ishamjotkaur" >  My Github Link </Link>
+              
+            </div>
+        </main>
+    )
+}
