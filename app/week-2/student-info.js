8779e6805378ce98183ed6db732f47e0732cba64
ishamjotkaur <ishamjotkaur17@gmail.com>
2025-01-21 13:20:03 -0700

    Completed Root Page

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,13 +1,11 @@
 import Link from 'next/link';
-export default function studentInfo () {
-      
-    return (
-        <main>
-            <div>
-                <p> Ishamjot Kaur</p>
-                <Link href="https://github.com/ishamjotkaur" >  My Github Link </Link>
-              
-            </div>
-        </main>
-    )
-}
+export default function StudentInfo () {
+return (
+    <main>
+            <p> Ishamjot Kaur</p>
+        <p> <Link href="https://github.com/ishamjotkaur" >  My Github Link </Link> </p> 
+        <p> <Link href="/" >  Go back </Link> </p> 
+
+    </main>
+);
+}   
