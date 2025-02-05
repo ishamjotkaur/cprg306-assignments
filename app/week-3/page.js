d6be425550f89ba67902926d69f57edb2392277a
ishamjotkaur <ishamjotkaur17@gmail.com>
2025-02-04 19:00:02 -0700

    Added week-3

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,9 @@
 import ItemList from "./item-list";
-
 export default function Page(){
     return (
         <main>
             <h1 className="text-4xl font-bold m-6 text-center text-blue-400"> Shopping List </h1>
-           <ItemList/>
+           <ItemList />
         </main>
     );
 }
